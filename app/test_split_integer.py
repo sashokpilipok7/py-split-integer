9eb450cbbbb995ef58d8d6aec8a12e9a9118ea5e
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2026-06-26 23:40:44 +0300

    feat: solution with dynamic and simple versions

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,16 +1,5 @@
 from app.split_integer import split_integer
 
-def test_base_logic_for_each_split() -> None:
-    value = 20
-    parts = 3
-    result = split_integer(value, parts)
-
-    assert sum(result) == value
-    assert len(result) == parts
-    assert max(result) - min(result) <= 1
-    assert sorted(result) == result
-
-
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     value = 11
     parts = 5
@@ -18,6 +7,8 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
     assert sum(res) == value
+    assert res == [2,2,2,2,3]
+
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -28,6 +19,8 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     for n in res:
         assert value // parts == n, "parrts not equal"
 
+    assert res == [4,4,4,4,4]
+
 
 
 
@@ -35,6 +28,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     res = split_integer(10,1)
 
     assert res[0] == 10
+    assert res == [10]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
