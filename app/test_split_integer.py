750a6e02ba355cb1e94f734062c8da6cac0fec84
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2026-06-27 18:23:58 +0300

    fix: after review

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,34 +2,20 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    res = split_integer(11, 5)
-
-    assert sum(res) == 11
-    assert res == [2, 2, 2, 2, 3]
+    assert sum(split_integer(11, 5)) == 11
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    res = split_integer(20, 5)
-    for number in res:
-        assert 20 // 5 == number, "parrts not equal"
-    assert res == [4, 4, 4, 4, 4]
+    assert split_integer(10, 2) == [5, 5]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    res = split_integer(10, 1)
-
-    assert res[0] == 10
-    assert res == [10]
+    assert split_integer(10, 1) == [10]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    res = split_integer(20, 3)
-
-    assert res == sorted(res)
-    assert res == [6, 7, 7]
+    assert sorted(split_integer(20, 3)) == [6, 7, 7]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    res = split_integer(10, 15)
-
-    assert res.count(0) == 5
+    assert split_integer(2, 5).count(0) == 3
